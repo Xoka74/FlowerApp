ca0cf10076ce00f21363f99c7a752fdebefc126b
Vladimir Boyko <ya.vladimir565@mail.ru>
2024-11-20 04:15:26 +0500

    Added forgotten import

--- ui_kit/build.gradle.kts
+++ ui_kit/build.gradle.kts
@@ -32,4 +32,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    debugImplementation(libs.androidx.ui.tooling)
+    implementation(libs.androidx.activity.compose)
 }
