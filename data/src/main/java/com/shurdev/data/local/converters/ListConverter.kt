61fa4147666b61c87769e23375c1a0d175596f76
fed1v <fedivmi@gmail.com>
2024-11-26 19:19:08 +0500

    Renamed functions in ListConverter

--- data/src/main/java/com/shurdev/data/local/converters/ListConverter.kt
+++ data/src/main/java/com/shurdev/data/local/converters/ListConverter.kt
@@ -10,13 +10,13 @@ class ListConverter {
     private val gson = Gson()
 
     @TypeConverter
-    fun fromString(value: String?): List<String> {
+    fun fromJson(value: String?): List<String> {
         val listType = object : TypeToken<List<String>>() {}.type
         return gson.fromJson(value, listType)
     }
 
     @TypeConverter
-    fun fromArrayList(list: List<String>): String {
+    fun toJson(list: List<String>): String {
         val json = gson.toJson(list)
         return json
     }
